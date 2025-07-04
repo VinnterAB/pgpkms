9bff75721390876b6481aff6e55de262b636eaf9
Oscar Hellström <oscar.hellstrom@vinnter.se>
2025-07-04 11:14:47 +0200

    Don't call AWS unless we need to
    Check that we know how to export the key before fetching it.

--- cmd/export.go
+++ cmd/export.go
@@ -8,11 +8,6 @@ import (
 )
 
 func ExportKey(client kms.Client, opts *Opts, args []string) error {
-	key, err := client.GetKey(*opts.Export)
-	if err != nil {
-		return err
-	}
-
 	var name, comment, email string
 	if opts.ExportName != nil {
 		name = *opts.ExportName
@@ -27,6 +22,11 @@ func ExportKey(client kms.Client, opts *Opts, args []string) error {
 	// Check that we have at least name or email
 	if name == "" && email == "" {
 		return fmt.Errorf("at least one of --export-name or --export-email must be provided")
+	}
+
+	key, err := client.GetKey(*opts.Export)
+	if err != nil {
+		return err
 	}
 
 	bytes, err := pgp.Export(key.PublicKey, key.PrivateKey, opts.Armor, name, comment, email)
