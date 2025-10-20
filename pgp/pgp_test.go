64d6dd7732725d0d5e542b423e199b4114f14c19
Tom Victor <vjtomvictor@icloud.com>
2025-10-20 12:01:18 +0200

    go-crypto test fixes

--- pgp/pgp_test.go
+++ pgp/pgp_test.go
@@ -2,9 +2,8 @@ package pgp
 
 import (
 	"crypto"
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
 	"crypto/x509"
 	"io"
 	"strings"
@@ -12,19 +11,19 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/kms"
-	"github.com/aws/aws-sdk-go-v2/service/kms/types"
+	kmsTypes "github.com/aws/aws-sdk-go-v2/service/kms/types"
 	kmslib "github.com/vinnterab/pgpkms/kms"
 	"gotest.tools/v3/assert"
 )
 
-// MockSigner implements crypto.Signer for testing
+// MockSigner implements crypto.Signer using an RSA key for testing
 type MockSigner struct {
-	privateKey *ecdsa.PrivateKey
+	privateKey *rsa.PrivateKey
 }
 
 func NewMockSigner() *MockSigner {
-	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	return &MockSigner{privateKey: privateKey}
+	privKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	return &MockSigner{privateKey: privKey}
 }
 
 func (m *MockSigner) Public() crypto.PublicKey {
@@ -36,22 +35,19 @@ func (m *MockSigner) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts)
 }
 
 func TestExport(t *testing.T) {
-	// Create a mock KMS public key
 	mockSigner := NewMockSigner()
-	pubKey := mockSigner.Public().(*ecdsa.PublicKey)
+	pubKey := mockSigner.Public().(*rsa.PublicKey)
 
-	// Marshal the public key for the mock KMS data
 	pubKeyBytes, err := x509.MarshalPKIXPublicKey(pubKey)
 	assert.NilError(t, err)
 
-	// Create mock KMS key data
 	keyId := "test-key-id"
 	keyArn := "arn:aws:kms:us-east-1:123456789012:key/test-key-id"
 	creationDate := time.Now()
 
 	kmsPublicKey := &kmslib.PublicKey{
 		Description: &kms.DescribeKeyOutput{
-			KeyMetadata: &types.KeyMetadata{
+			KeyMetadata: &kmsTypes.KeyMetadata{
 				KeyId:        &keyId,
 				Arn:          &keyArn,
 				CreationDate: &creationDate,
@@ -60,24 +56,19 @@ func TestExport(t *testing.T) {
 		Key: &kms.GetPublicKeyOutput{
 			KeyId:     &keyId,
 			PublicKey: pubKeyBytes,
-			KeySpec:   types.KeySpecEccNistP256,
+			KeySpec:   kmsTypes.KeySpec("RSA_2048"), // Fixed KeySpec
 		},
 	}
 
 	t.Run("Export with custom user ID", func(t *testing.T) {
 		result, err := Export(kmsPublicKey, mockSigner, false, "John Doe", "Test Comment", "john@example.com")
 		assert.NilError(t, err)
-		assert.Assert(t, result != nil)
 		assert.Assert(t, result.Len() > 0)
 	})
 
 	t.Run("Export with ASCII armor", func(t *testing.T) {
 		result, err := Export(kmsPublicKey, mockSigner, true, "John Doe", "Test Comment", "john@example.com")
 		assert.NilError(t, err)
-		assert.Assert(t, result != nil)
-		assert.Assert(t, result.Len() > 0)
-
-		// Check for PGP armor headers
 		output := result.String()
 		assert.Assert(t, strings.Contains(output, "-----BEGIN PGP PUBLIC KEY BLOCK-----"))
 		assert.Assert(t, strings.Contains(output, "-----END PGP PUBLIC KEY BLOCK-----"))
@@ -86,15 +77,13 @@ func TestExport(t *testing.T) {
 	t.Run("Export with default user ID", func(t *testing.T) {
 		result, err := Export(kmsPublicKey, mockSigner, false, "", "", "")
 		assert.NilError(t, err)
-		assert.Assert(t, result != nil)
 		assert.Assert(t, result.Len() > 0)
 	})
 }
 
 func TestSerialize(t *testing.T) {
-	// Create a mock entity for testing serialization
 	mockSigner := NewMockSigner()
-	pubKey := mockSigner.Public().(*ecdsa.PublicKey)
+	pubKey := mockSigner.Public().(*rsa.PublicKey)
 
 	pubKeyBytes, err := x509.MarshalPKIXPublicKey(pubKey)
 	assert.NilError(t, err)
@@ -105,7 +94,7 @@ func TestSerialize(t *testing.T) {
 
 	kmsPublicKey := &kmslib.PublicKey{
 		Description: &kms.DescribeKeyOutput{
-			KeyMetadata: &types.KeyMetadata{
+			KeyMetadata: &kmsTypes.KeyMetadata{
 				KeyId:        &keyId,
 				Arn:          &keyArn,
 				CreationDate: &creationDate,
@@ -114,16 +103,13 @@ func TestSerialize(t *testing.T) {
 		Key: &kms.GetPublicKeyOutput{
 			KeyId:     &keyId,
 			PublicKey: pubKeyBytes,
-			KeySpec:   types.KeySpecEccNistP256,
+			KeySpec:   kmsTypes.KeySpec("RSA_2048"), // Fixed KeySpec
 		},
 	}
 
 	t.Run("Serialize without armor", func(t *testing.T) {
 		result, err := Export(kmsPublicKey, mockSigner, false, "Test User", "", "test@example.com")
 		assert.NilError(t, err)
-		assert.Assert(t, result != nil)
-
-		// Should be binary data without armor
 		output := result.String()
 		assert.Assert(t, !strings.Contains(output, "-----BEGIN PGP PUBLIC KEY BLOCK-----"))
 	})
@@ -131,9 +117,6 @@ func TestSerialize(t *testing.T) {
 	t.Run("Serialize with armor", func(t *testing.T) {
 		result, err := Export(kmsPublicKey, mockSigner, true, "Test User", "", "test@example.com")
 		assert.NilError(t, err)
-		assert.Assert(t, result != nil)
-
-		// Should have armor headers
 		output := result.String()
 		assert.Assert(t, strings.Contains(output, "-----BEGIN PGP PUBLIC KEY BLOCK-----"))
 		assert.Assert(t, strings.Contains(output, "-----END PGP PUBLIC KEY BLOCK-----"))
