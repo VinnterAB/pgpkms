47663314b77c6f2ed08e06059203c7e24b8c3f6f
Oscar Hellström <oscar.hellstrom@vinnter.se>
2026-03-16 11:23:29 +0100

    We're in 2026

--- cmd/version.go
+++ cmd/version.go
@@ -4,7 +4,7 @@ func VersionString() string {
 	return `gpg (GnuPG) 2.4.4
 libgcrypt 1.10.3
 pgpkms (github.com/vinnterab/pgpkms)
-Copyright (C) 2025 Vinnter AB
+Copyright (C) 2026 Vinnter AB
 License MIT <https://opensource.org/licenses/MIT>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
